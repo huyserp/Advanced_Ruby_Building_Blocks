893e3583139a6bd5538ab7a5011124bb4c6c93a9
Peter Huyser <huyserp@gmail.com>
2020-04-13 13:43:37 +0200

    completed assignment 1

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -26,4 +26,38 @@ def bubble_sort(array)
     return array  
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+def bubble_sort_by(array)
+    shifting = true
+    
+    while shifting
+        shift_count = 0
+        working_counter = 0
+
+        i = 0
+        while i < (array.length - 1)
+            if yield(array[i], array[(i + 1)]) > 0
+                array[i], array[(i + 1)] = array[(i + 1)], array[i]
+                working_counter += 1
+            end
+            i += 1
+        end
+
+        if shift_count != working_counter
+            shift_count = working_counter
+            i = 0
+        else
+            shifting = false
+        end
+
+    end
+
+    return array  
+end
+
+puts puts 
+puts bubble_sort([22,10,0,12,2,1,7,150,4,3,78,2,0,2])
+
+puts puts
+
+puts bubble_sort_by(["bonjour","hola","hi","hello","hey"]) { |left,right| left.length - right.length }
+puts puts
