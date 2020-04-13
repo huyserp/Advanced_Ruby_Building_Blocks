d6c9b15eeee1e184f722c803cff831ec1e53129d
Peter Huyser <huyserp@gmail.com>
2020-04-13 14:41:46 +0200

    minor changes to bubble sort methods...

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,9 @@
 def bubble_sort(array)
     shifting = true
-    
+
     while shifting
         shift_count = 0
         working_counter = 0
-
         i = 0
         while i < (array.length - 1)
             if array[i] > array[(i + 1)]
@@ -13,26 +12,22 @@ def bubble_sort(array)
             end
             i += 1
         end
-
         if shift_count != working_counter
             shift_count = working_counter
             i = 0
         else
             shifting = false
         end
-
     end
-
     return array  
 end
 
 def bubble_sort_by(array)
     shifting = true
-    
+
     while shifting
         shift_count = 0
         working_counter = 0
-
         i = 0
         while i < (array.length - 1)
             if yield(array[i], array[(i + 1)]) > 0
@@ -41,23 +36,15 @@ def bubble_sort_by(array)
             end
             i += 1
         end
-
         if shift_count != working_counter
             shift_count = working_counter
             i = 0
         else
             shifting = false
         end
-
     end
-
     return array  
-end
+end 
 
-puts puts 
 puts bubble_sort([22,10,0,12,2,1,7,150,4,3,78,2,0,2])
-
-puts puts
-
 puts bubble_sort_by(["bonjour","hola","hi","hello","hey"]) { |left,right| left.length - right.length }
-puts puts
