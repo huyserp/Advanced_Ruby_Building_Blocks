b59b90aa69ba7acf694718988518db614751f80c
Peter Huyser <huyserp@gmail.com>
2020-04-15 14:07:26 +0200

    working any? all? none? methods

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -35,38 +35,49 @@ module Enumerable
         while i < self.length
             if yield(self[i]) != true
                 checker += 1
-            else
-                false
+                return false
             end
             i += 1
         end
-        if checker != 0
-            true
-        end
+        checker == 0 ? true : false
     end
 
     def my_any?
         checker = 0
         i = 0
         while i < self.length
-            yield(self[i]) != true ? next : checker += 1
+            if yield(self[i]) == true
+                checker += 1
+                true
+            end
             i += 1
         end
         checker == 0 ? false : true
     end
 
     def my_none?
+        checker = 0
         i = 0
         while i < self.length
             if yield(self[i]) == true
-                return false
-                break
+                checker += 1
+                false 
             end
             i += 1
         end
+        checker == 0 ? true : false
     end
 
-    def my_count
+    def my_count(arg=nil)
+        
+        if arg == nil
+            return self.length
+        elsif arg != nil
+
+
+            
+        end
+
     end
 
     def my_map
